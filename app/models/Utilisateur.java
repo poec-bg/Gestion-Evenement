7de46c9c4b8adec8c7868fd0549203030119eb2c
hill <hilal.ouhab@gmail.com>
2016-10-03 11:43:09 +0200

    Création de la classe Utilisateur

---
+++ app/models/Utilisateur.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by Administrateur on 03/10/2016.
+ */
+public class Utilisateur {
+    public String email;
+    public String nom;
+    public String prenom;
+    public String motDePasse;
+}
