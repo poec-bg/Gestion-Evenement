8719cc833e772eccfd4cfb33ec6b1f15c7f135d2
hill <hilal.ouhab@gmail.com>
2016-10-03 14:38:31 +0200

    création classe utilisateur et dossier service

---
+++ app/services/UtilisateurService.java
@@ -0,0 +1,25 @@
+package services;
+
+/**
+ * Created by Administrateur on 03/10/2016.
+ */
+public class UtilisateurService {
+
+    /**
+     * Singleton
+     */
+    private static UtilisateurService instance;
+
+    /**
+     * Constructeur privé = personne ne peut faire de new UtilisateurService()
+     */
+    private UtilisateurService() {
+    }
+
+    public static UtilisateurService get() {
+        if (instance == null) {
+            instance = new UtilisateurService();
+        }
+        return instance;
+    }
+}
