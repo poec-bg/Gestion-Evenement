a805b598441c6380b42730f4b6b582574a16d02f
hill <hilal.ouhab@gmail.com>
2016-10-04 14:49:58 +0200

    - ajout package exceptions - ajout package type - ajout package Emailvalidator - ajout méthode : create, listUtilisateur, remove -Mise à jour classe Utilisateur

---
+++ app/models/types/EUserRole.java
@@ -0,0 +1,17 @@
+package models.types;
+
+public enum EUserRole {
+
+
+    ADMIN("Administrateur"),CLIENT("Client Normal");
+
+    private String label;
+
+    EUserRole(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
--- app/services/UtilisateurService.java
+++ app/services/UtilisateurService.java
@@ -1,9 +1,23 @@
 package services;
 
-/**
- * Created by Administrateur on 03/10/2016.
- */
+
+import exceptions.InvalidArgumentException;
+import models.Utilisateur;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import validators.EmailValidator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+
 public class UtilisateurService {
+
+
+    private List<Utilisateur> utilisateurs;
 
     /**
      * Singleton
@@ -14,6 +28,7 @@ public class UtilisateurService {
      * Constructeur privé = personne ne peut faire de new UtilisateurService()
      */
     private UtilisateurService() {
+        utilisateurs = new ArrayList<>();
     }
 
     public static UtilisateurService get() {
@@ -21,5 +36,92 @@ public class UtilisateurService {
             instance = new UtilisateurService();
         }
         return instance;
+    }
+
+    // Création d'un nouvel utilisateur: email et mots de passe requis
+
+    public Utilisateur create(String email, String motDePasse) throws Exception {
+
+        List<String> validationMessages = new ArrayList<>();
+        if (email == null || email.equals("")) {
+            validationMessages.add("Le email ne peut être null ou vide");
+        } else {
+            if (!EmailValidator.validate(email)) {
+                validationMessages.add("Le format de l'email est invalide");
+            }
+        }
+        if (motDePasse == null || motDePasse.equals("")) {
+            validationMessages.add("Le motDePasse ne peut être null ou vide");
+        }
+        if (validationMessages.size() > 0) {
+            throw new InvalidArgumentException((String[]) validationMessages.toArray(new String[0]));
+        }
+
+        Utilisateur utilisateur=new Utilisateur();
+        utilisateur.email = email;
+        utilisateur.motDePasse=motDePasse;
+
+        Session session = HibernateUtils.getSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            session.save(utilisateur);
+            tx.commit();
+        }catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            throw new Exception("HibernateException: " + e.getMessage() );
+        }finally {
+            session.close();
+        }
+        return utilisateur;
+
+    }
+
+      // Lister les utilisateurs
+
+    public void listUtilisateurs( ){
+        Session session = HibernateUtils.getSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            List utilisateurs = session.createQuery("FROM Utilisateur ").list();
+            for (Iterator iterator =
+                 utilisateurs.iterator(); iterator.hasNext();){
+                Utilisateur utilisateur = (Utilisateur) iterator.next();
+                System.out.print("Email: " + utilisateur.email);
+                System.out.print("  Nom: " + utilisateur.nom);
+                System.out.println("  Prenom: " + utilisateur.prenom);
+            }
+            tx.commit();
+        }catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+    }
+
+
+      // Supprimer un utilisateur
+    public void remove(Integer UtilisateurID){
+        Session session = HibernateUtils.getSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            Utilisateur utilisateur =
+                    (Utilisateur)session.get(Utilisateur.class, UtilisateurID);
+            session.delete(utilisateur);
+            tx.commit();
+        }catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+    }
+
+
+    public void clear() {
+        utilisateurs = new ArrayList<>();
     }
 }
