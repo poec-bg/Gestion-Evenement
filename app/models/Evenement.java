54a30442945e318b6796abaf651d77668c32fc11
AimeDeb <aime.debonne@gmal.com>
2016-10-04 10:18:52 +0200

    modif class Evenement : ajout hibernate

--- app/models/Evenement.java
+++ app/models/Evenement.java
@@ -2,13 +2,20 @@ package models;
 
 import org.joda.time.DateTime;
 
+import javax.persistence.*;
+
+@Entity
 public class Evenement {
+    @Id
     public long id;
     public String idCreateur;
     public String nom;
     public String description;
+    @Temporal(TemporalType.TIMESTAMP)
     public DateTime dateDebut;
+    @Temporal(TemporalType.TIMESTAMP)
     public DateTime dateFin;
-    public Invite[] invites;
     public String lieu;
+    @OneToMany(cascade={CascadeType.ALL})
+    public Invite[] invites;
 }
